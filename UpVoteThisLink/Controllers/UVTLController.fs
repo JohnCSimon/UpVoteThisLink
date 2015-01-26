b20f9da39a3ea16ad5cc10abf0a9a6a595cb24a4
mylittletestuser <johncsimon@gmail.com>
2015-01-26 00:22:09 -0800

    Validation.

--- UpVoteThisLink/Controllers/UVTLController.fs
+++ UpVoteThisLink/Controllers/UVTLController.fs
@@ -29,12 +29,10 @@ type UVTLController() =
     let fromUpVoteTable q = fromTable tableClient "UpVoteThisLinkStorage" q
 
     let getUpVotesForShortKey shortKey = 
-            Query.all<UpVoteRecord>
-            |> Query.where <@ fun g s -> s.PartitionKey = shortKey @>
-            |> fromUpVoteTable
+        Query.all<UpVoteRecord>|> Query.where <@ fun g s -> s.PartitionKey = shortKey @> |> fromUpVoteTable
 
     let doUpVoteByShortkey shortKey =
-        let queryResult = Query.all<UpVoteRecord> |> Query.where <@ fun g s -> s.PartitionKey = shortKey @> |> fromUpVoteTable 
+        let queryResult = getUpVotesForShortKey shortKey
         if queryResult |> Seq.length = 1 then
             let (upvoteRecord, metadata) = queryResult |> Seq.exactlyOne
             let modifiedGame = { upvoteRecord with UpVotes = upvoteRecord.UpVotes + 1}
@@ -43,10 +41,16 @@ type UVTLController() =
         else 
             {PartitionKey = ""; RowKey = ""; Url = ""; UpVotes = 0 }
 
-    let insertByUrl url username = 
-        let uvr = {PartitionKey = string(url.GetHashCode()); RowKey = username; Url = url; UpVotes = 0 }
-        let result = uvr |> Insert |> inUpVoteTable
-        result
+    let insertByUrl url username =
+        let shortKey = string(url.GetHashCode());
+        let checkRecordAlreadyExists = getUpVotesForShortKey shortKey
+        if checkRecordAlreadyExists |> Seq.length = 0 then
+            let upvoteRecord = {PartitionKey = string(url.GetHashCode()); RowKey = username; Url = url; UpVotes = 0 }
+            let result = upvoteRecord |> Insert |> inUpVoteTable 
+            upvoteRecord
+        else
+            { PartitionKey = ""; RowKey = ""; Url = ""; UpVotes = 0 }
+            
 
     member x.Get() = "you get nothing!"
     
@@ -55,4 +59,3 @@ type UVTLController() =
     member x.Post(key: string) = doUpVoteByShortkey key 
     
     member x.Put(url: string, username) = insertByUrl url username
-
