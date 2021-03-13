ca48a2fdd926bc308a0fbb753a615750e91e2dcd
John Simon <johncsimon@gmail.com>
2021-03-12 23:58:54 -0800

    more test code

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,9 +1,38 @@
+use chrono::{Datelike, NaiveDate, NaiveDateTime, Timelike, Utc};
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::App;
+use actix_web::{
+    get, post,
+    web::{self, Json},
+    HttpResponse, HttpServer, Responder,
+};
+
+#[derive(Deserialize)]
+pub struct MyInput {
+    username: String,
+}
+
+impl MyInput {
+    fn getTime(&self) -> String {
+        let now = Utc::now();
+        let (is_common_era, year) = now.hour12();
+
+        let y = format!(
+            "{}, The current UTC date is {}-{:02}-{:02} {:?}",
+            self.username,
+            year,
+            now.month(),
+            now.day(),
+            now.weekday()
+        );
+        return y;
+    }
+}
 
 #[get("/")]
-pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn hello(infoo: Json<MyInput>) -> impl Responder {
+    let _x = MyInput::getTime(&infoo.0);
+    HttpResponse::Ok().body("Hello world!".to_string() + &infoo.username + &_x)
 }
 
 #[post("/echo")]
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 mod endpoints;
-
+#[macro_use]
+extern crate serde_derive;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 // use endpoints;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Started");
     HttpServer::new(|| {
         App::new()
             .service(endpoints::hello)
             .service(endpoints::echo)
         // .route("/hey", web::get().to(manual_hello))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:18080")?
     .run()
     .await
 }
