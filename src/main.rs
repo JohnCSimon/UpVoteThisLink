11ba1b32f83f3a09d4f01ff0c9af1096817e68bb
John Simon <johncsimon@gmail.com>
2022-03-10 00:41:40 -0800

    post
    post

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-mod url;
-
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
+pub mod urlparser;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let x = urlparser::parse_url_local("https://www.rust-lang.org/").unwrap();
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/hey", web::post().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
-
 #[get("/")]
 async fn hello() -> impl Responder {
+    println!("POOP");
     HttpResponse::Ok().body("Hootie toot!")
 }
 
@@ -26,5 +28,7 @@ async fn echo(req_body: String) -> impl Responder {
 }
 
 async fn manual_hello() -> impl Responder {
+    println!("POOP");
+
     HttpResponse::Ok().body("Hey there!")
 }
