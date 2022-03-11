f0ba2bf82a49bf853dfdf458659f9b63c09e0f80
John Simon <johncsimon@gmail.com>
2022-03-11 00:12:39 -0800

    emotes
    debug trait
    
    debug trait

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,41 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
+// use serde::Deserialize;
+// use upvotes::VoteEvent;
 
+pub mod upvotes;
 pub mod urlparser;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let x = urlparser::parse_url_local("https://www.rust-lang.org/").unwrap();
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-            .route("/hey", web::post().to(manual_hello))
+        App::new().service(hello)
+        //.service(echo)
+        // .route("/hey", web::get().to(manual_hello))
+        // .route("/hey", web::post().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
+
 #[get("/")]
 async fn hello() -> impl Responder {
-    println!("POOP");
     HttpResponse::Ok().body("Hootie toot!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+// #[derive(Deserialize)]
+// struct Info {
+//     username: String,
+// }
 
-async fn manual_hello() -> impl Responder {
-    println!("POOP");
+// async fn index(info: web::Json<Info>) -> Result<String, String> {
+//     Ok(format!("Welcome {}!", info.username))
+// }
 
-    HttpResponse::Ok().body("Hey there!")
-}
+// #[post("/vote")]
+// async fn echo(vote_event: web::Json<VoteEvent>) -> Result<String, String> {
+//     Ok(format!("Welcome {}!", vote_event.urlId))
+// }
+
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
---
+++ src/upvotes.rs
@@ -0,0 +1,88 @@
+pub use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct VoteEvent {
+    pub url_id: String,
+    pub user_id: String,
+    pub event: Event,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub enum Event {
+    UpVote,
+    DownVote,
+}
+
+// enum UpvoteError {
+//     InvalidUrlId,
+//     InvalidUserId,
+//     DuplicateUpvote,
+// }
+
+// enum Emotes {
+//     Smile,
+//     Frown,
+//     Meh,
+//     Wink,
+//     Tongue,
+//     Grin,
+//     Sad,
+//     Cry,
+//     Surprise,
+//     Angry,
+//     Confused,
+//     Cool,
+//     Heart,
+//     Diamond,
+//     Star,
+//     Bomb,
+//     Skull,
+//     Coffee,
+//     Ghost,
+//     Soccer,
+//     Diamond2,
+//     Anchor,
+//     Soccer2,
+//     Bomb2,
+//     Skull2,
+//     Coffee2,
+//     Ghost2,
+//     Diamond3,
+// }
+
+#[cfg(test)]
+mod tests {
+    use crate::urlparser::parse_url_local;
+
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn deserialize() -> Result<(), String> {
+        let point = VoteEvent {
+            url_id: "https://www.rust-lang.org/".to_string(),
+            user_id: "12345".to_string(),
+            event: Event::down_vote,
+        };
+
+        let serialized = serde_json::to_string(&point).unwrap();
+        println!("serialized = {}", serialized);
+
+        let deserialized: VoteEvent = serde_json::from_str(&serialized).unwrap();
+        println!("deserialized = {:?}", deserialized);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_url_fail() -> Result<(), String> {
+        let badurl = "httpss//www.rust-lang.org/";
+        match parse_url_local(badurl) {
+            Ok(_) => Err("Should have failed".to_string()),
+            Err(x) => {
+                println!("Error: {}", &x);
+                Ok(())
+            }
+        }
+    }
+}
