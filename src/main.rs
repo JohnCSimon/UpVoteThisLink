2b6e15cf26df39a4c35e3d31d5340169fc0ef8a3
John Simon <johncsimon@gmail.com>
2022-08-20 00:12:28 -0700

    further work

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,76 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-// use serde::Deserialize;
-// use upvotes::VoteEvent;
-pub mod sqlstuff;
 pub mod upvotes;
 pub mod urlparser;
-#[actix_web::main]
+
+use actix_web::{get, web, App, HttpServer, Responder, post};
+use serde::Deserialize;
+
+// use crate::upvotes::VoteEvent;
+
+// #[post("/vote")]
+async fn echo(vote_event: web::Json<upvotes::VoteEvent>) -> Result<String, String> {
+    // Ok(format!("Welcome {}!", vote_event.urlId))
+    Ok(format!("Hello !!!"))
+}
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!!!")
+}
+
+
+#[post("/hey/{name}")]
+async fn greet2(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!!!!!")
+}
+
+#[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(hello)
-        //.service(echo)
-        // .route("/hey", web::get().to(manual_hello))
-        // .route("/hey", web::post().to(manual_hello))
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+            .service(greet2)
+
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hootie toot!")
-}
-
-// #[derive(Deserialize)]
-// struct Info {
-//     username: String,
+// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+// use serde::Deserialize;
+// use upvotes::VoteEvent;
+// pub mod sqlstuff;
+// pub mod upvotes;
+// pub mod urlparser;
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new().service(hello)
+//         // .service(echo)
+//         .route("/hey", web::get().to(manual_hello))
+//         .route("/hey", web::post().to(manual_hello))
+//     })
+//     .bind(("127.0.0.1", 8080))?
+//     .run()
+//     .await
 // }
+
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hootie toot!")
+// }
+
+
 
 // async fn index(info: web::Json<Info>) -> Result<String, String> {
 //     Ok(format!("Welcome {}!", info.username))
 // }
 
-// #[post("/vote")]
-// async fn echo(vote_event: web::Json<VoteEvent>) -> Result<String, String> {
-//     Ok(format!("Welcome {}!", vote_event.urlId))
-// }
+// // #[post("/vote")]
+// // async fn echo(vote_event: web::Json<VoteEvent>) -> Result<String, String> {
+// //     Ok(format!("Welcome {}!", vote_event.urlId))
+// // }
 
 // async fn manual_hello() -> impl Responder {
 //     HttpResponse::Ok().body("Hey there!")
